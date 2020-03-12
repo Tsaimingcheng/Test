6a067edb9ee635b01df20d91265cae071febfd13
tsaimingcheng <isu10503014a@cloud.isu.edu.tw>
2020-03-13 02:23:59 +0800

    update

--- app/src/main/java/com/example/caculationtest/TitleFragment.java
+++ app/src/main/java/com/example/caculationtest/TitleFragment.java
@@ -5,7 +5,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.SavedStateViewModelFactory;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.navigation.NavAction;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import com.example.caculationtest.databinding.FragmentTitleBinding;
 
 
 /**
@@ -19,9 +29,23 @@ public class TitleFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        MyViewModel myViewModel;
+        myViewModel = ViewModelProviders.of(requireActivity(),new SavedStateViewModelFactory(requireActivity().getApplication(),this)).get(MyViewModel.class);
+        FragmentTitleBinding binding;
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_title,container,false);
+        binding.setData(myViewModel);
+        binding.setLifecycleOwner(requireActivity());
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NavController controller = Navigation.findNavController(v);
+                controller.navigate(R.id.action_titleFragment_to_questionFragment);
+            }
+        });
+        return binding.getRoot();
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_title, container, false);
+        //return inflater.inflate(R.layout.fragment_title, container, false);
     }
 }
